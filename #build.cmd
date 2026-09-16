5a40a682033385d3ac786dc6b5ce0d4f14d744ea
xiao1994start <xiao1994start@live.com>
2026-09-16 09:24:18 +0800

    R2026-09-16_9-24-17

--- #build.cmd
+++ #build.cmd
@@ -18,7 +18,7 @@ set "PATH=%NODE_HOME%;%systemPATH%;"
 
 ECHO 环境测试
 ECHO 升级 npm
-call npm i --global npm --registry https://registry.npmmirror.com/
+call npm install --global npm --registry https://registry.npmmirror.com/
 ECHO Node.js 版本号:
 call node -v
 ECHO NPM 版本号:
