da8797f30547ddd1b72cf1fbba2d32a44e9c9a00
xiao1994start <xiao1994start@live.com>
2026-09-17 09:37:19 +0800

    R2026-09-17_9-37-15

--- #count_snippets_json.cmd
+++ #count_snippets_json.cmd
@@ -5,24 +5,53 @@ CD /D "%~dp0"
 
 @REM  统计 snippets 目录下所有 json 文件的数量
 set /a "count=0"
+@REM  全局类JSON 文件数量
+set /a "total_json=0"
+@REM  nodeJS/TS类JSON 文件数量
+set /a "js_ts_json=0"
 
-for /R "%~dp0snippets" %%i in ("*.json") do ( set /a "count+=1" )
+for /R "%~dp0snippets" %%i in ("*.json") do (
+    set "file_name=%%~fi"
+    if not "!file_name:全局=!"=="!file_name!" (
+        set /a "total_json+=1"
+    ) else if not "!file_name:nodeJS=!"=="!file_name!" (
+        if not "!file_name:bat=!"=="!file_name!" (
+            @REM  包含 bat 字符串的 JSON 文件
+            set /a "count+=1"
+        ) else (
+            @REM  不包含 bat 字符串的 JSON 文件
+            set /a "js_ts_json+=1"
+        )
+    ) else (
+        set /a "count+=1"
+    )
+)
+
+ECHO.全局json文件数量: !total_json!
+ECHO.nodeJS/TS json文件数量: !js_ts_json!
 
 
 @REM  每个 json 文件占用的行数倍数
-set /a "mult=4"
+set /a "base_mult=4"
+
+
 @REM  需要重复计数的 json 文件数量
-set /a "dup=4"
+@REM  = 全局类JSON * 6 + nodeJS/TS类JSON * 2
+set /a "dup=total_json*6+js_ts_json*2"
+
+
 @REM  忽略的 json 文件数量
 set /a "ign=3"
 
+
 @REM  开始行号
-set /a "start_line=83"
+set /a "start_line=105"
 @REM  结束补充行数
 set /a "end_line=4"
 
+
 @REM  计算结束行号
-set /a "end_line=start_line+end_line+(count+dup-ign)*mult"
+set /a "end_line=start_line+end_line+(count+dup-ign)*base_mult"
 
 
 for /f %%a in ('find /c /v "" ^< "%~dp0package.json"') do set "raw=%%a"
@@ -31,11 +60,13 @@ set /a "total_lines+=1"
 
 
 ECHO ====================== 统计 ======================
-@REM  ECHO 统计到的 json 文件数量: !count!
-@REM  ECHO 统计到的 json 文件数量: !start_line! 到 !end_line!
+ECHO 统计到的 json 文件数量: !count!
+ECHO 统计到的 json 文件数量: !start_line! 到 !end_line!
+ECHO.
 ECHO.统计数: !end_line!
 ECHO.总行数: !total_lines!
 
 
 
 endlocal
+@REM  renpy-syntaxes 更新地址 : https://github.com/renpy/vscode-language-renpy
